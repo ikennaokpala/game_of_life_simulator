e56bd4033685b9a26edfd5b8d560795950057de2
Ikenna Okpala <me@ikennaokpala.com>
2015-10-13 05:46:45 +0100

    Test valid?

--- spec/opinion_spec.rb
+++ spec/opinion_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 describe Opinion do
 
+  let(:opinion){:soft}
+  let(:neighbours){[:soft, :soft, :none, :none]}
   describe "#find" do
-    let(:opinion){:soft}
-    let(:neighbours){[:soft, :soft, :none, :none]}
     context "when a block is given" do
       it "returns a none empty collection" do
         expect(Opinion.new(opinion, neighbours).find{|op| op ==:soft}).to eq(2)
@@ -14,6 +14,21 @@ describe Opinion do
     context "when a block is not given" do
       it "returns and empty array if block is not given" do
         expect(Opinion.new(opinion, neighbours).find).to eq([])
+      end
+    end
+  end
+
+  describe "#valid?" do
+    context "when the opinion is hard or soft " do
+      it "returns true" do
+        expect(Opinion.new(:hard, neighbours).valid?).to eq(true)
+        expect(Opinion.new(:soft, neighbours).valid?).to eq(true)
+      end
+    end
+
+    context "when the opinion is not hard or soft" do
+      it "returns false" do
+        expect(Opinion.new(:none, neighbours).valid?).to eq(false)
       end
     end
   end
